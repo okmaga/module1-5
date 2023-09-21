9c2cdfc6a436c3d7858e395af8c1b4724238b34e
Maga Bashirov <maga.bashirov@gmail.com>
2023-09-21 22:24:48 +0300

    fix: henlo

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,5 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main>Commit 5</main>;
+  return <main>Commit 5 Henlo</main>;
 }
