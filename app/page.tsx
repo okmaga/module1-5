0fb7d9f37da0a16346ff6399212db26ea1ef8591
Maga Bashirov <maga.bashirov@gmail.com>
2023-09-24 11:19:09 +0300

    feat: title changed

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,9 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main>Commit 5 Henlo</main>;
+  return (
+    <main>
+      <h1>Title changed</h1>
+    </main>
+  );
 }
